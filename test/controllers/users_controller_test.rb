3120b4f80c0910b2f203f516396e5e278870a30f
LastOne817 <lastone817@naver.com>
2015-04-20 19:32:53 +0900

    Test signup Failure

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,29 @@ class UsersControllerTest < ActionController::TestCase
         assert json['user_name'] == 'testuser'
         assert json['login_count'] == 1
     end
+
+    test '#signup_failure_user' do
+        post :signup, {'username' => 'test', 'password' => 'testpass'}
+        json = JSON.parse(response.body)
+        assert json['error_code'] == -1
+    end
+
+    test '#signup_failure_pwd' do
+        post :signup, {'username' => 'testuser', 'password' => 'test'}
+        json = JSON.parse(response.body)
+        assert json['error_code'] == -2
+    end
+
+    test '#signup_failure_dup' do
+        post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+        json = JSON.parse(response.body)
+        assert json['user_name'] == 'testuser'
+        assert json['login_count'] == 1
+
+        post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+        json = JSON.parse(response.body)
+        assert json['error_code'] == -3
+    end
   # test "the truth" do
   #   assert true
   # end
